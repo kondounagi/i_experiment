2af6f901855cf28967393796c704b43775254c9a
S.Shibata <sshiba274@gmail.com>
2019-07-09 16:19:38 +0900

    fixed

--- I2/i1i2i3_phone2.c
+++ I2/i1i2i3_phone2.c
@@ -66,7 +66,7 @@ void read_data_thread(struct communicate_set *set){
         int n = read(0, set->data, sizeof(set->data));
         if (n > 0) {
             int snd = send(set->socket, set->data, sizeof(set->data), 0);
-            fprintf(stderr, "sended\n");
+
             if(snd == -1) {
                 fprintf(stderr, "send error\n");
                 exit(0);
@@ -81,7 +81,7 @@ void recieve_data_thread(struct communicate_set *set){
         int r = recv(set->socket, set->data, sizeof(set->data), 0);
         if (r > 0) {
             int wrt = write(1, set->data, sizeof(set->data));
-            fprintf(stderr, "recieved\n");
+
             if(wrt == -1){
                 fprintf(stderr, "write error!\n");
                 exit(0);
@@ -105,65 +105,40 @@ int main(int argc, char* argv[]){
         fprintf(stderr, "please check args\n");
     }
 
-    //int n, r;
-        // sending 1 bite
-       // n = read(0, send_data, sizeof(send_data));
+    pthread_t sendthread, recievethread;
+    int ret1, ret2;
 
-
-        pthread_t sendthread, recievethread;
-        int ret1, ret2;
-
-        struct communicate_set send_set;
-        send_set.socket = s;
+    struct communicate_set send_set;
+    send_set.socket = s;
         
 
-        struct communicate_set recieve_set;
-        recieve_set.socket = s;
+    struct communicate_set recieve_set;
+    recieve_set.socket = s;
 
-        ret1 = pthread_create(&sendthread, NULL, (void *)read_data_thread, &send_set);
-        ret2 = pthread_create(&recievethread, NULL, (void *)recieve_data_thread, &recieve_set);
+    ret1 = pthread_create(&sendthread, NULL, (void *)read_data_thread, &send_set);
+    ret2 = pthread_create(&recievethread, NULL, (void *)recieve_data_thread, &recieve_set);
 
-        if(ret1 !=0){
-            fprintf(stderr, "failed to create sendthread\n");
-            exit(0);
-        }
-        if(ret2 !=0){
-            fprintf(stderr, "failed to create recievethread\n");
-            exit(0);
-        }
+    if(ret1 !=0){
+        fprintf(stderr, "failed to create sendthread\n");
+        exit(0);
+    }
+    if(ret2 !=0){
+        fprintf(stderr, "failed to create recievethread\n");
+        exit(0);
+    }
 
-         ret1 = pthread_join(sendthread,NULL);
-         fprintf(stderr, "ret1(send) thread started \n");
-         ret2 = pthread_join(recievethread,NULL);
-         fprintf(stderr, "ret2(recv) thread started \n");
-        if(ret1 !=0){
-            fprintf(stderr, "failed to join sendthread\n");
-            exit(0);
-        }
-        if(ret2 !=0){
-            fprintf(stderr, "failed to join recievethread\n");
-            exit(0);
-        }
-        ///read_data_thread(send_data, ps);
-        // if (n > 0) {
-        //     int snd = send(s, send_data, sizeof(send_data), 0);
-        //     if(snd == -1) {
-        //         fprintf(stderr, "send error\n");
-        //         exit(0);
-        //     }
-        //     // fprintf(stderr, "sending successed.\n");
-        // }
-        ///recieve_data_thread(recieved_data, ps);
-        // recieving 1 bite
-        // r = recv(s, recieved_data, sizeof(recieved_data), 0);
-        // if (r > 0) {
-        //     int wrt = write(1, recieved_data, sizeof(recieved_data));
-        //     if(wrt == -1){
-        //         fprintf(stderr, "write error!\n");
-        //         exit(0);
-        //     }
-        //     // fprintf(stderr, "recieving successed.\n");
-        // }
+     ret1 = pthread_join(sendthread,NULL);
+     fprintf(stderr, "ret1(send) thread started \n");
+     ret2 = pthread_join(recievethread,NULL);
+     fprintf(stderr, "ret2(recv) thread started \n");
+    if(ret1 !=0){
+        fprintf(stderr, "failed to join sendthread\n");
+        exit(0);
+    }
+    if(ret2 !=0){
+        fprintf(stderr, "failed to join recievethread\n");
+        exit(0);
+    }
     
     fprintf(stderr, "datasend finished.\n");
     close(s);

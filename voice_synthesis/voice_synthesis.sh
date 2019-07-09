cb6d98802f9c140bfddbac6320b84a49416fdf69
kondouagi <kondouagi@gmail.com>
2019-07-09 14:59:13 +0900

    added voice_synthesis.sh

--- voice_synthesis/voice_synthesis.sh
+++ voice_synthesis/voice_synthesis.sh
@@ -5,4 +5,5 @@ open_jtalk \
 -x /var/lib/mecab/dic/open-jtalk/naist-jdic \
 -m /usr/share/hts-voice/mei/mei_normal.htsvoice \
 -r 1.0 \
--ow open_jtalk.raw
+-ow open_jtalk.raw && \
+aplay open_jtalk.raw

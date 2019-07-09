ded693d55f5b32486626fe58f794a62b4cd48761
kondouagi <kondouagi@gmail.com>
2019-07-09 15:14:06 +0900

    fixed voice_synthesis.sh

--- voice_synthesis/voice_synthesis.sh
+++ voice_synthesis/voice_synthesis.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-
-echo $1 | \
-open_jtalk \
+if [ -p /dev/stdin ]; then
+    cat -
+else
+    echo $@
+fi | open_jtalk \
 -x /var/lib/mecab/dic/open-jtalk/naist-jdic \
 -m /usr/share/hts-voice/mei/mei_normal.htsvoice \
 -r 1.0 \
--ow open_jtalk.raw && \
-aplay open_jtalk.raw
+-ow /dev/stdout \
+| aplay
